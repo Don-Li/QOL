f7ec537dcf9f08c19fc23445ff705b43a9f3fc9f
Don Li <yli877@aucklanduni.ac.nz>
2020-09-13 14:38:36 +1200

    It is my birthday

---
+++ R/open_explorer.R
@@ -0,0 +1,28 @@
+#' Open explorer
+#'
+#' @param path 
+#'
+#' @return
+#' No return values. This function is invoked for the side-effect of opening Windows 
+#' explorer at a given directory.
+#' 
+#' @export open_explorer
+#'
+#'
+#'
+#' @examples
+#' \dontrun{
+#' # Open Windows explorer in the current directory
+#' open_explorer()
+#' # Open windows explorer in your desktop
+#' open_explorer("C:/whoami/Desktop")
+#' }
+#' 
+open_explorer = function( path = NULL ){
+    if ( is.null(path) ){
+        cur_wd = normalizePath(path)
+    } else{
+        cur_wd = normalizePath(getwd())
+    }
+    system2( "explorer", args = cur_wd, invisible = FALSE, minimized = F )
+}
